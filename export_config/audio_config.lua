74f70a67e760e7849a74903b5dd37e2936231916
duchao <duchao@qq.com>
2021-12-08 09:34:59 +0800

    修改音效名字

---
+++ export_config/audio_config.lua
@@ -0,0 +1,167 @@
+return {
+	game=
+	{
+		bgm_main_hall=
+		{
+			name = "bgm_main_hall",
+			audio_name = "bgm_main_hall.mp3",
+			desc = "大厅背景音乐，循环",
+		},
+		com_but_confirm=
+		{
+			name = "com_but_confirm",
+			audio_name = "com_but_confirm.mp3",
+			desc = "确认，选择",
+		},
+		com_but_cancel=
+		{
+			name = "com_but_cancel",
+			audio_name = "com_but_cancel.mp3",
+			desc = "取消",
+		},
+	},
+	ludo=
+	{
+		ludo_usually_BGM=
+		{
+			name = "ludo_usually_BGM",
+			audio_name = "ludo_usually_BGM.mp3",
+			desc = "通常BGM",
+		},
+		ludo_throw_dice=
+		{
+			name = "ludo_throw_dice",
+			audio_name = "ludo_throw_dice.mp3",
+			desc = "投掷骰子",
+		},
+		ludo_chess_jump=
+		{
+			name = "ludo_chess_jump",
+			audio_name = "ludo_chess_jump.mp3",
+			desc = "棋子移动",
+		},
+		ludo_chess_small_drop=
+		{
+			name = "ludo_chess_small_drop",
+			audio_name = "ludo_chess_small_drop.mp3",
+			desc = "棋子踩中安全点",
+		},
+		ludo_chess_big_drop=
+		{
+			name = "ludo_chess_big_drop",
+			audio_name = "ludo_chess_big_drop.mp3",
+			desc = "棋子踩中对手棋子",
+		},
+		ludo_round_start=
+		{
+			name = "ludo_round_start",
+			audio_name = "ludo_round_start.mp3",
+			desc = "自己的回合开始",
+		},
+		ludo_countdown=
+		{
+			name = "ludo_countdown",
+			audio_name = "ludo_countdown.mp3",
+			desc = "10秒倒计时结束前3秒(音频文件只有1秒）",
+		},
+		ludo_coin_appear=
+		{
+			name = "ludo_coin_appear",
+			audio_name = "ludo_coin_appear.mp3",
+			desc = "金币出现",
+		},
+		ludo_coin_get=
+		{
+			name = "ludo_coin_get",
+			audio_name = "ludo_coin_get.mp3",
+			desc = "金币获得",
+		},
+		ludo_to_end=
+		{
+			name = "ludo_to_end",
+			audio_name = "ludo_to_end.mp3",
+			desc = "进入终点",
+		},
+		ludo_chess_chose=
+		{
+			name = "ludo_chess_chose",
+			audio_name = "ludo_chess_chose.mp3",
+			desc = "多个棋子可选择时",
+		},
+	},
+	domino=
+	{
+		bgm_duominuo_bj=
+		{
+			name = "bgm_duominuo_bj",
+			audio_name = "bgm_duominuo_bj.mp3",
+			desc = "背景音乐 ",
+		},
+		bgm_duominuo_kaishi=
+		{
+			name = "bgm_duominuo_kaishi",
+			audio_name = "bgm_duominuo_kaishi.mp3",
+			desc = "开始  ",
+		},
+		bgm_duominuo_fapai=
+		{
+			name = "bgm_duominuo_fapai",
+			audio_name = "bgm_duominuo_fapai.mp3",
+			desc = "发牌",
+		},
+		bgm_duominuo_luopai=
+		{
+			name = "bgm_duominuo_luopai",
+			audio_name = "bgm_duominuo_luopai.mp3",
+			desc = "落牌",
+		},
+		bgm_duominuo_timeout=
+		{
+			name = "bgm_duominuo_timeout",
+			audio_name = "bgm_duominuo_timeout.mp3",
+			desc = "时间快到了",
+		},
+		bgm_duominuo_pass=
+		{
+			name = "bgm_duominuo_pass",
+			audio_name = "bgm_duominuo_pass.mp3",
+			desc = "pass音效",
+		},
+		bgm_duominuo_jinbi=
+		{
+			name = "bgm_duominuo_jinbi",
+			audio_name = "bgm_duominuo_jinbi.mp3",
+			desc = "金币",
+		},
+		bgm_duominuo_lose=
+		{
+			name = "bgm_duominuo_lose",
+			audio_name = "bgm_duominuo_lose.mp3",
+			desc = "lose",
+		},
+		bgm_duominuo_win=
+		{
+			name = "bgm_duominuo_win",
+			audio_name = "bgm_duominuo_win.mp3",
+			desc = "win",
+		},
+		bgm_duominuo_jiesuan1=
+		{
+			name = "bgm_duominuo_jiesuan1",
+			audio_name = "bgm_duominuo_jiesuan1.mp3",
+			desc = "结算音效（1，2倍用",
+		},
+		bgm_duominuo_jiesuan2=
+		{
+			name = "bgm_duominuo_jiesuan2",
+			audio_name = "bgm_duominuo_jiesuan2.mp3",
+			desc = "结算音效（3倍以上）",
+		},
+		bgm_duominuo_fanpai=
+		{
+			name = "bgm_duominuo_fanpai",
+			audio_name = "bgm_duominuo_fanpai.mp3",
+			desc = "翻牌",
+		},
+	},
+}
